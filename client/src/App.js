8241bb3a44b5b611c33d4b2f1f23c62f7c43304b
Mariano170395 <mercadolibremariano17@gmail.com>
2021-06-21 17:06:52 -0500

    [ADD] Created mariano branch and tested a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Fraggy from './components/Fraggy';
+import Mariano from './components/Mariano';
 
 function App() {
   return (
     <div className="App">
       <Fraggy />
+      <Mariano/>
     </div>
   );
 }
