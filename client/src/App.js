219690e9fb6f663a38d9ebd4b6798009a898779d
TonyAnaya <jaanaya99@gmail.com>
2021-06-21 17:29:47 -0500

    [ADD] Se le agrego la parte de Tony

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Fraggy from './components/Fraggy';
+import Tony from './components/Tony';
 
 function App() {
   return (
     <div className="App">
       <Fraggy />
+      <Tony />
     </div>
   );
 }
