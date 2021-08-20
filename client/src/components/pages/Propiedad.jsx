878c3d857da78b1435597fb226cc72dea17b0d13
Mariano Estrada <mariano-e-s@hotmail.com>
2021-08-20 15:08:05 -0500

    avance gral

--- client/src/components/pages/Propiedad.jsx
+++ client/src/components/pages/Propiedad.jsx
@@ -1,35 +1,52 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Propiedad = () => {
+  const [image, setImage] = useState({
+    title: "",
+    url: "",
+    _id: "",
+  });
+  const params = useParams();
 
-    const [image, setImage] = useState({
-        title:"",
-        url:"",
-        _id:"",
-    })
-    const params = useParams()
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(`/api/images/${params.id}`);
+      setImage(res.data);
+    })();
+  });
 
-    useEffect(() => {
-        (async() =>{
-            const res = await axios.get(`/api/images/${params.id}`)
-            setImage(res.data)
-        })();
-    });
-
-    return (
-        <div>
-            <img src={image.url} alt={image.title}></img>
-            <h1>{image.title}</h1>
-            <h1>{image.prioridad}</h1>
-            <h1>{image.descripcion}</h1>
-            <h1>{image.banos}</h1>
-            <h1>{image.habitaciones}</h1>
-            <h1>{image.precio}</h1>
-            <h1>{image.colonia}</h1>
+  return (
+    <div>
+      <div className="Separador"></div>
+      <div className='PropiedadWrapper'>
+        <img src={image.url} alt={image.title} className="Casa"></img>
+        <div className="InformacionProp">
+            <div className='IsotipoProp'>
+        <div className="InfoGrande">
+        <h1 className='TituloProp'>{image.title}</h1>
+        <h1 className="PrecioProp">{`$${image.precio}`}</h1>
         </div>
-    )
-}
+        <div className="InfoChica">
+        <h1 className="BanosProp">{`Baños: ${image.banos}`}</h1>
+        <h1 className="HabitacionesProp">{`Habitaciones: ${image.habitaciones}`}</h1>
+        </div>
+        </div>
+        </div>
+        <div className="Separador"></div>
+        <div>
 
-export default Propiedad
+        </div>
+      </div>
+      <h1>{image.prioridad}</h1>
+      <h1>{image.descripcion}</h1>
+      
+      
+      <h1>{image.precio}</h1>
+      <h1>{image.colonia}</h1>
+    </div>
+  );
+};
+
+export default Propiedad;
